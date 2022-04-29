3e46d91fecc698d1328272c5aed5769dcf0b512a
Agussangari <agustin.sangari@gmail.com>
2022-04-29 12:09:51 -0300

    routes and created addlist component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  // {
+  //   path: 'agregar',
+  //   loadChildren: () => import('./pages/add/add.module').then( m => m.AddPageModule)
+  // }
 ];
 @NgModule({
   imports: [
