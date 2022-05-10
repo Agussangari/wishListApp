594674e0d1860d0286651a740147ac90298f8cb5
Agussangari <agustin.sangari@gmail.com>
2022-05-10 11:44:00 -0300

    creada la funcion de agregar deseos o tareas en la lista

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { WishesService } from 'src/app/services/wishes.service';
 
 @Component({
@@ -10,12 +11,44 @@ import { WishesService } from 'src/app/services/wishes.service';
 export class Tab1Page {
 
   constructor(public wishesService: WishesService,
-              private router: Router) {
+              private router: Router, private alertCtrl: AlertController) {
 
                }
-  addList(){
-    
-    this.router.navigateByUrl('/tabs/tab1/agregar');
+  async addList(){
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Nueva Lista',
+      inputs: [
+        {
+          name: 'title',
+          type:'text',
+          placeholder: 'Nombre de la lista'
+      }
+    ],
+      buttons: [
+        {
+          text: 'Canelar',
+          role:'cancel',
+          handler:() => {
+            console.log('Canelar');
+          }
+        },
+        {
+          text: 'Crear',
+          handler: (data) => {
+            console.log(data);
+            if(data.title.lenght === 0){
+              return;
+            }
+            this.wishesService.createList(data.title);
+            //tengo que crear la lista
+          }
+        }
+      ]
+    });
+
+     alert.present();
+    // this.router.navigateByUrl('/tabs/tab1/agregar');
 
   }
 
--- src/app/services/wishes.service.ts
+++ src/app/services/wishes.service.ts
@@ -15,4 +15,16 @@ export class WishesService {
     this.lists.push(list1, list2);
     
    }
+
+    createList( title: string) {
+      const newList = new List(title);
+      this.lists.push(newList);
+      
+    }
+
+
+
+
+
+
 }
